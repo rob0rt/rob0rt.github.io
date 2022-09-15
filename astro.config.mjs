67d36b23e583c5e1d5a1b6acc262f431c44f80f7
Robert Lynch <robertlynch2012@gmail.com>
2022-09-15 11:32:22 -0700

    Use tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import svelte from "@astrojs/svelte";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte()]
+  integrations: [mdx(), sitemap(), svelte(), tailwind()]
 });
