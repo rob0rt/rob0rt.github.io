d42dbd49eb8ac2bc00886eb5d77db4a8c15e9777
Robert Lynch <robertlynch2012@gmail.com>
2024-05-09 00:28:29 -0700

    Migrate the background to be vanilla

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import svelte from "@astrojs/svelte";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://robertlyn.ch',
-  integrations: [mdx(), sitemap(), svelte(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()]
 });
